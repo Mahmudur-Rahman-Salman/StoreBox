7f1e6bb909bd4f782939a89d1bac2b69c8f894b1
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2024-11-24 01:14:44 +0600

    signin signup form created

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-    <div className="flex-center h-screen">
-      <h1 className="h1">StoreBox - The only storage solution you need</h1>
-    </div>
-  );
+    return (
+        <div className="flex-center h-screen">
+            <h1 className="h1">StoreBox - The only storage solution you need</h1>
+        </div>
+    );
 }
